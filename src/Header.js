c481459a641b9e6885037220ccbf54e471116716
nathalero <110853338+nathalero@users.noreply.github.com>
2022-12-22 16:50:03 +0100

    Create Header.js
    new app for header was created

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+<div className="Header">
+<header className="Header">
+    <h1>
+    Weather App
+    </h1>
+    </header>    
+</div>
+  );
+}
+
+export default Header;
