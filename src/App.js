38266039eb48ccceac569a0c7a89cfe437d3fa7a
nathalero <110853338+nathalero@users.noreply.github.com>
2022-12-22 16:50:29 +0100

    Update App.js
    app.js was edited

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>
-          Weather App with React
-     </h1>
-          </header>
+
       <footer>
-      <p>Built By <a href="https://www.linkedin.com/in/nathalero/" target="_blank">Nathalie Lesmes Rodriguez</a> <br/> with React <br/> My <a href="https://github.com/nathalero" target="_blank"> Github</a> Repository</p>
+      <p>This project was coded By <a href="https://www.linkedin.com/in/nathalero/" target="_blank">Nathalie Lesmes Rodriguez</a> and is <a href="https://github.com/nathalero" target="_blank"> open-source on Github</a></p>
       </footer>
     </div>
   );
