b5aee9a004dcfad8d6301cf144b408ed24b90762
nathalero <110853338+nathalero@users.noreply.github.com>
2023-01-01 18:21:41 +0100

    Update Weather.js
    Axios was added and used by include the weather APIs

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(){
-    return (
-        <div className="Weather">
-            <div className="container">
-            <form>
-                <div className="row">
-                    <div className="col-9">
-                        <input
-                        type="search"
-                        placeholder="Enter a city..."
-                        className="form-control"
-                        autoFocus="on"/>
-                    </div>
-                    <div className="col-3">
-                        <input
-                        type="submit"
-                        value="Search" 
-                        className="btn btn-primary w-100"/>
-                    </div>
-                </div>
-            </form>
+    const [weatherData, setWeatherData] = useState({ ready: false });
+  
+    function handleResponse(response){
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            date: "Wednesday 07:00"
+        });
+    }
 
-            <h1>New York</h1>
-            <ul>
-                <li> Wednesday</li>
-                <li>Mostly Cloudy</li>
-            </ul>
-            <div className="row mt-3">
-                <div className="col-6">
-                    <img 
-                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                    alt="Mostly Cloudy"/>
-                   <span className="temperature">6</span> 
-                   <span className="unit">°C</span>
+    if (weatherData.ready){
+        return (
+            <div className="Weather">
+                <div className="container">
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input
+                            type="search"
+                            placeholder="Enter a city..."
+                            className="form-control"
+                            autoFocus="on"/>
+                        </div>
+                        <div className="col-3">
+                            <input
+                            type="submit"
+                            value="Search" 
+                            className="btn btn-primary w-100"/>
+                        </div>
+                    </div>
+                </form>
+    
+                <h1>{weatherData.city}</h1>
+                <ul>
+                    <li> {weatherData.date} </li>
+                    <li className="text-capitalize">{weatherData.description}</li>
+                </ul>
+                <div className="row mt-3">
+                    <div className="col-6">
+                        <img 
+                        src={weatherData.iconUrl}
+                        alt={weatherData.description}/>
+                       <span className="temperature">{Math.round(weatherData.temperature)}</span> 
+                       <span className="unit">°C</span>
+                    </div>
+                    <div className="col-6">
+                        <ul>
+                            <li> Humidity: {weatherData.humidity}%</li>
+                            <li> Wind: {weatherData.wind} Km/h</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="col-6">
-                    <ul>
-                        <li> Precipitation: 15%</li>
-                        <li> Huminidity: 72%</li>
-                        <li> Wind: 13 Km/h</li>
-                    </ul>
-                </div>
-            </div>
+        </div>
     </div>
-</div>
-)
+    )
+    }
+
+    else {
+        
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let city = `New York`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+    }
+
 }
